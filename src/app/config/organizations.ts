0be2e5832b8f0401ab78283af0d8244e3f5d9b60
Yahya Gilany <yahya.gilany@live.com>
2026-01-20 11:42:20 -0500

    Modify organization mappings for IT3049C
    Updated organization mappings for IT3049C and removed IT2053C.

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -1,22 +1,11 @@
 // Organization and assignment mappings
 export const ORGANIZATIONS = {
-  "IT3049C-Fall25": {
-    name: "IT3049C Fall25",
+  "IT3049C-Spring26": {
+    name: "IT3049C Spring26",
     owner: "IT3049C", // GitHub organization/owner name
     assignments: {
-
-      "🧑‍💻Lab: Git Started - Your Profile": "git-started-with-you-profile",
-      "🧑‍💻 Campus Portal (JS Exercises)": "js-exercise-campus-portal",
-      "🧑‍💻 Game Lobby With Rock Paper Scissors": "Game-Lobby-With-Rock-Paper-Scissors"
-    }
-  },
-  "IT2053C-Fall25": {
-    name: "IT2053C Fall25",
-    owner: "IT2053C-Gilany", // GitHub organization/owner name
-    assignments: {
-      "Env Setup Check": "env-setup-check",
-      "Python Bootcamp": "python-bootcamp",
-      "Lists and Tuples": "lists-and-tuples",
+      "🧑‍💻Lab: Git Started - Your Profile": "git-started-with-your-profile",
+      "🧑‍💻 Campus Portal (JS Exercises)": "campus-portal-js-exercises"
     }
   }
 } as const;
